4b43e934c3a723f9361df849b27e75278397c5ad
pollettoimpanato <david200288l@gmail.com>
2022-03-09 08:49:51 +0100

    todo & allTodo queries

--- src/resolvers/todo.resolver.ts
+++ src/resolvers/todo.resolver.ts
@@ -1,9 +1,15 @@
-import { Query, Resolver } from "type-graphql";
+import TodoEntity from "../entities/todo.entity";
+import { Arg, Int, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export default class TodoResolver {
-    @Query(() => String)
-    hello(): string {
-        return "hello, world";
+    @Query(() => TodoEntity, { nullable: true })
+    todo(@Arg("id", () => Int) id: number): Promise<TodoEntity | undefined> {
+        return TodoEntity.findOne({ id });
+    }
+
+    @Query(() => [TodoEntity])
+    allTodos(): Promise<TodoEntity[]> {
+        return TodoEntity.find({});
     }
 }
