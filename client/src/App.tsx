0770901060412f6d8f64a6b3ac5808127e28b0aa
pollettoimpanato <david200288l@gmail.com>
2022-03-16 10:42:01 +0100

    add graphql and urql

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,33 @@
 import { BsFillTrashFill, BsPlusLg } from "react-icons/bs";
 import Todo from "./components/Todo";
+import { createClient, Provider } from "urql";
+
+const client = createClient({ url: "http://localhost:8000/graphql" });
 
 const App = () => {
     return (
-        <div className="flex justify-center flex-col h-screen items-center">
-            <div className="flex flex-col bg-blue-700 p-2 rounded-lg ring-1 ring-black w-1/3">
-                <div className="flex justify-between p-2 items-center">
-                    <input
-                        type="text"
-                        placeholder="Enter a todo..."
-                        className="w-3/4 bg-white rounded-lg focus:ring-white focus:ring-1 p-1 outline-none"
-                    />
-                    <div className="grid grid-cols-2 gap-3">
-                        <BsPlusLg className="cursor-pointer text-white"></BsPlusLg>
-                        <BsFillTrashFill className="cursor-pointer text-white"></BsFillTrashFill>
+        <Provider value={client}>
+            <div className="flex justify-center flex-col h-screen items-center">
+                <div className="flex flex-col bg-blue-700 p-2 rounded-lg ring-1 ring-black w-1/3">
+                    <div className="flex justify-between p-2 items-center">
+                        <input
+                            type="text"
+                            placeholder="Enter a todo..."
+                            className="w-3/4 bg-white rounded-lg focus:ring-white focus:ring-1 p-1 outline-none"
+                        />
+                        <div className="grid grid-cols-2 gap-3">
+                            <BsPlusLg className="cursor-pointer text-white"></BsPlusLg>
+                            <BsFillTrashFill className="cursor-pointer text-white"></BsFillTrashFill>
+                        </div>
+                    </div>
+                    <div className="mt-4">
+                        <Todo id={1} text="todo"></Todo>
+                        <Todo id={1} text="todo"></Todo>
+                        <Todo id={1} text="todo"></Todo>
                     </div>
                 </div>
-                <div className="mt-4">
-                    <Todo id={1} text="todo"></Todo>
-                    <Todo id={1} text="todo"></Todo>
-                    <Todo id={1} text="todo"></Todo>
-                </div>
             </div>
-        </div>
+        </Provider>
     );
 };
 
